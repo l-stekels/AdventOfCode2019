2f99f7ffe85834506682d60aaf0c1538b610b286
l-stekels <l.stekels@gmail.com>
2019-12-09 10:27:31 +0200

    day4 part one done

---
+++ day4_part1.py
@@ -0,0 +1,31 @@
+rangeStart = 136760
+rangeEnd = 595730
+
+
+def getDigits(number):
+    return [int(x) for x in str(number)]
+
+
+def sameAdjecent(number):
+    digits = getDigits(number)
+    prev = 0
+    for digit in digits:
+        if prev == digit:
+            return True
+        prev = digit
+    return False
+
+
+def growing(number):
+    digits = getDigits(number)
+    for i, j in enumerate(digits[:-1]):
+        if j > digits[i+1]:
+            return False
+    return True
+
+
+result = []
+for number in range(rangeStart, rangeEnd):
+    if sameAdjecent(number) and growing(number):
+        result.append(number)
+print(len(result))
